4387db9d9b1fea7fb0a6f38d20a9a0d3a511a940
João <joaocdvancea@gmail.com>
2024-04-02 17:30:53 -0300

    exercicio 2: funcao adicionar aprimorada, funcao limpar concluida

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,18 +1,24 @@
 var precoTotal = 0;
+var precoTotalHTML = document.querySelector('#valor-total');
+var carrinho = document.querySelector('.carrinho__produtos__produto');
 
 function adicionar() {
     var produtoCompleto = document.querySelector('#produto').value;
+    var produtoValores = produtoCompleto.split(' - R$');
     var produtoQuantidade = document.querySelector('#quantidade').value;
-    var produtoValores = produtoCompleto.split('R$');
-    var precoTotalHTML = document.querySelector('#valor-total');
 
     var precoProduto = produtoValores[1] * produtoQuantidade;
-    var produtoNome = produtoValores[0].split('' -'');
     precoTotal += precoProduto;
 
-    var carrinho = document.querySelector('.carrinho__produtos__produto');
-    var novoItem = `<br><span class="texto-azul">${produtoQuantidade}x</span> ${produtoNome[0]} <span class="texto-azul">R$${produtoValores[1]}</span>`;
+    var novoItem = `<br><span class="texto-azul">${produtoQuantidade}x</span> ${produtoValores[0]} <span class="texto-azul">R$${produtoValores[1]}</span>`;
 
     carrinho.innerHTML += novoItem;
     precoTotalHTML.innerHTML = `R$${precoTotal}`;
+}
+
+function limpar() {
+    precoTotalHTML.innerHTML = 'R$0';
+    produtoQuantidade = '';
+    document.querySelector('#quantidade').value = '';
+    carrinho.innerHTML = '';
 }
