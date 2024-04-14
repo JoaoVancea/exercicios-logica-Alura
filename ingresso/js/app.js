519c0f4838b0cfb4f79ce72d3dfb84fd045f32da
João <joaocdvancea@gmail.com>
2024-04-13 23:42:36 -0300

    exercicio 3 concluido

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,13 +1,9 @@
 function comprar() {
-    var ingresso = document.querySelector('#tipo-ingresso').value;
+    var ingresso = document.querySelector('#tipo-ingresso');
     var quantidade = document.querySelector('#qtd').value;
     var lis = document.querySelector('.lista').getElementsByTagName('li');
     var quantidades = document.querySelector('.lista').getElementsByTagName('span');
-
-    var listaQuantidades = [];
-        for(var i = 0; i < lis.length; i++) {
-            listaQuantidades.push(quantidades[i].innerText);
-        }
+    var tipoIngresso = ingresso.options[ingresso.selectedIndex].innerText;
 
     var listaCompleta = [];
     for(var i = 0; i < lis.length; i++) {
@@ -17,5 +13,15 @@ function comprar() {
     var listaProdutos = [];
     for(var i = 0; i < listaCompleta.length; i++) {
         listaProdutos.push(listaCompleta[i].replace(/[0-9]/g, ''));
+    }
+
+    for(var i = 0; i < listaProdutos.length; i++) {
+        if(tipoIngresso == listaProdutos[i]) {
+            if(parseInt(quantidades[i].innerText) <  parseInt(quantidade)) {
+                alert('Não há estoque suficiente');
+            } else {
+                quantidades[i].innerText = (parseInt(quantidades[i].innerText) - parseInt(quantidade));
+            }
+        }
     }
 }
